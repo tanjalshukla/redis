9aa4cf521bbff2bbef1dc82323c2e52be54a9332
tshukl <tshukl@uw.edu>
2026-07-12 11:09:30 -0700

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -7,40 +8,43 @@ import java.net.InetSocketAddress;
 
 
 public class Main {
+  // server port
+  private final static int PORT = 6379;
+
   public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
-
-    //  Uncomment the code below to pass the first stage
-    ServerSocket serverSocket = null;
-    Socket clientSocket = null;
-    int port = 6379;
-    try {
-      serverSocket = new ServerSocket();
-      // Since the tester restarts your program quite often, setting SO_REUSEADDR
-      // ensures that we don't run into 'Address already in use' errors
+    try (ServerSocket serverSocket = new ServerSocket()) {
+      // setting reuse address before bind to bypass timeouts from prev connections
       serverSocket.setReuseAddress(true);
-      serverSocket.bind(new InetSocketAddress("localhost", port));
-      // Wait for connection from client.
-      clientSocket = serverSocket.accept();
+      serverSocket.bind(new InetSocketAddress("localhost", PORT));
 
+      // continuously accept new connections
+      while (true) {
+        // Wait for connection from client.
+        Socket clientSocket = serverSocket.accept();
+        // dispatch handler in vthread
+        Thread vThread = Thread.startVirtualThread(() ->
+        {
+          handleClient(clientSocket);
+        });
+      }
+    } catch (IOException e) {
+      System.err.println("Could not listen on port: " + PORT);
+    }
+  }
+
+  /** Handles multiple commands from one client thru virtual thread */
+  private static void handleClient(Socket clientSocket) {
+    try (clientSocket) {
       DataInputStream in = new DataInputStream(clientSocket.getInputStream());
       byte[] buffer = new byte[1024];
       int bytesRead;
       while ((bytesRead = in.read(buffer)) != -1) {
         clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
       }
-
+    } catch (EOFException _) {
+      // ignore, client finished
     } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
-    } finally {
-        try {
-          if (clientSocket != null) {
-            clientSocket.close();
-          }
-        } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
-        }
+      System.out.println("Client Exception " + e.getMessage());
     }
   }
 }
